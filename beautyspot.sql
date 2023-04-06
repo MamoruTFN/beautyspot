ea2cef7278e50932268284d32131814d90cbb7e4
Peerawat Wongmek <78299267+peerawat2645@users.noreply.github.com>
2023-04-06 13:11:49 +0700

    Update beautyspot.sql

--- beautyspot.sql
+++ beautyspot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2023 at 08:15 PM
+-- Generation Time: Apr 06, 2023 at 08:11 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `beautyspot`
 --
-CREATE DATABASE IF NOT EXISTS `beautyspot` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `beautyspot`;
+
 -- --------------------------------------------------------
 
 --
@@ -34,6 +33,19 @@ CREATE TABLE `customer` (
   `lastName` varchar(50) NOT NULL,
   `tel` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customerId`, `firstName`, `lastName`, `tel`) VALUES
+(1, 'string', 'string', 'string'),
+(2, 'string', 'string', 'string'),
+(3, 'asd', 'asdasd', '123123123'),
+(4, 'asdasd', 'asdasdas', '213123'),
+(5, 'วันนี้กินไรดี', 'ไม่รู้', '21312312'),
+(6, 'dasd', 'asdasdasd', '213123'),
+(7, 'asdasd', 'asdasdasda', '213123');
 
 -- --------------------------------------------------------
 
@@ -52,7 +64,7 @@ CREATE TABLE `district` (
 --
 
 INSERT INTO `district` (`DistrictID`, `ProvinceID`, `name`) VALUES
-(1, 1, 'peace2');
+(1, 1, 'เมือง');
 
 -- --------------------------------------------------------
 
@@ -77,14 +89,40 @@ CREATE TABLE `employee` (
 
 CREATE TABLE `payment` (
   `PaymentID` int(11) NOT NULL,
-  `PromotionID` int(11) NOT NULL,
   `ReservationID` int(11) NOT NULL,
-  `customerId` int(11) NOT NULL,
-  `type` enum('Atplace') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Atplace',
   `price` double NOT NULL,
-  `currentDate` date NOT NULL,
-  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `currentDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `price`
+--
+
+CREATE TABLE `price` (
+  `priceId` int(10) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `price`
+--
+
+INSERT INTO `price` (`priceId`, `name`, `amount`) VALUES
+(1, 'ตัดผม', 200),
+(2, 'สระไดร์', 150),
+(3, 'ทรีทเม้นท์', 250),
+(4, 'แต่งหน้า', 300),
+(5, 'ต่อเล็บ', 300),
+(6, 'ทาเล็บเจล', 200),
+(7, 'ทาเล็บธรรมดา', 100),
+(8, 'ทาเล็บเจลติดอะไหล่', 300),
+(9, 'นวดน้ำมัน', 400),
+(10, 'นวดแผนไทย', 250),
+(11, 'นวดอโรมา', 300),
+(12, 'นวดฝ้าเท้า', 250);
 
 -- --------------------------------------------------------
 
@@ -97,6 +135,15 @@ CREATE TABLE `promotion` (
   `percentDiscount` double NOT NULL,
   `PromotionID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `promotion`
+--
+
+INSERT INTO `promotion` (`name`, `percentDiscount`, `PromotionID`) VALUES
+('string', 0, 1),
+('string', 0, 2),
+('string', 0, 3);
 
 -- --------------------------------------------------------
 
@@ -114,7 +161,7 @@ CREATE TABLE `province` (
 --
 
 INSERT INTO `province` (`name`, `ProvinceID`) VALUES
-('peace', 1);
+('นครปฐม', 1);
 
 -- --------------------------------------------------------
 
@@ -125,13 +172,26 @@ INSERT INTO `province` (`name`, `ProvinceID`) VALUES
 CREATE TABLE `reservation` (
   `ReservationID` int(11) NOT NULL,
   `PromotionID` int(11) NOT NULL,
-  `StoreID` int(11) NOT NULL,
+  `StorePriceID` int(11) NOT NULL,
   `customerID` int(11) NOT NULL,
   `currentDate` date NOT NULL,
   `reservationDate` datetime NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `otherGroupId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`ReservationID`, `PromotionID`, `StorePriceID`, `customerID`, `currentDate`, `reservationDate`, `description`, `otherGroupId`) VALUES
+(1, 1, 1, 1, '2023-04-06', '2023-04-06 05:45:57', 'string', NULL),
+(2, 1, 1, 2, '2023-04-06', '2023-04-06 06:41:05', 'string', NULL),
+(3, 1, 1, 3, '2023-04-08', '2023-04-21 07:00:00', 'asdasd', NULL),
+(4, 1, 1, 4, '2023-04-08', '2023-04-13 07:00:00', 'asdasd', NULL),
+(5, 1, 1, 5, '2023-04-14', '2023-04-07 07:00:00', 'asdqwe213123', NULL),
+(6, 1, 1, 6, '2023-04-06', '2023-04-08 07:00:00', 'asdasdasd', NULL),
+(7, 1, 2, 7, '2023-04-08', '2023-04-28 07:00:00', 'asdasda', NULL);
 
 -- --------------------------------------------------------
 
@@ -168,7 +228,7 @@ CREATE TABLE `store` (
   `openTime` time NOT NULL DEFAULT '08:00:00',
   `closeTime` time NOT NULL DEFAULT '16:00:00',
   `phoneNumber` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `type` enum('Nail','Hair','Spa') COLLATE utf8_unicode_ci NOT NULL
+  `type` enum('ผม','เล็บ','นวด') COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -176,8 +236,28 @@ CREATE TABLE `store` (
 --
 
 INSERT INTO `store` (`StoreID`, `subdistrictID`, `number`, `road`, `name`, `openTime`, `closeTime`, `phoneNumber`, `type`) VALUES
-(1, 1, '1', '1', '1', '08:00:00', '16:00:00', '0896345911', 'Nail'),
-(2, 1, '2', '2', '2', '08:00:00', '16:00:00', '0896345911', 'Hair');
+(1, 1, '1', '1', '1', '08:00:00', '16:00:00', '0896345911', 'ผม'),
+(2, 1, '2', '2', '2', '08:00:00', '16:00:00', '0896345911', 'ผม');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `storeprice`
+--
+
+CREATE TABLE `storeprice` (
+  `storepriceId` int(11) NOT NULL,
+  `storeId` int(11) NOT NULL,
+  `priceId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `storeprice`
+--
+
+INSERT INTO `storeprice` (`storepriceId`, `storeId`, `priceId`) VALUES
+(1, 1, 1),
+(2, 2, 8);
 
 -- --------------------------------------------------------
 
@@ -196,7 +276,7 @@ CREATE TABLE `subdistrict` (
 --
 
 INSERT INTO `subdistrict` (`subdistrictID`, `DistrictID`, `name`) VALUES
-(1, 1, 'peace3');
+(1, 1, 'เมือง');
 
 --
 -- Indexes for dumped tables
@@ -229,10 +309,13 @@ ALTER TABLE `employee`
 ALTER TABLE `payment`
   ADD PRIMARY KEY (`PaymentID`),
   ADD UNIQUE KEY `TC_Payment103` (`ReservationID`),
-  ADD UNIQUE KEY `TC_Payment100` (`PromotionID`),
-  ADD KEY `TC_Payment116` (`PromotionID`),
-  ADD KEY `TC_Payment114` (`ReservationID`),
-  ADD KEY `TC_Payment115` (`customerId`);
+  ADD KEY `TC_Payment114` (`ReservationID`);
+
+--
+-- Indexes for table `price`
+--
+ALTER TABLE `price`
+  ADD PRIMARY KEY (`priceId`);
 
 --
 -- Indexes for table `promotion`
@@ -251,9 +334,10 @@ ALTER TABLE `province`
 --
 ALTER TABLE `reservation`
   ADD PRIMARY KEY (`ReservationID`),
+  ADD UNIQUE KEY `reservationDate` (`reservationDate`),
   ADD KEY `TC_Reservation120` (`customerID`),
   ADD KEY `TC_Reservation121` (`PromotionID`),
-  ADD KEY `TC_Reservation119` (`StoreID`);
+  ADD KEY `TC_Reservation119` (`StorePriceID`);
 
 --
 -- Indexes for table `role`
@@ -267,6 +351,14 @@ ALTER TABLE `role`
 ALTER TABLE `store`
   ADD PRIMARY KEY (`StoreID`),
   ADD KEY `TC_Store124` (`subdistrictID`);
+
+--
+-- Indexes for table `storeprice`
+--
+ALTER TABLE `storeprice`
+  ADD PRIMARY KEY (`storepriceId`),
+  ADD KEY `storeId` (`storeId`),
+  ADD KEY `priceId` (`priceId`);
 
 --
 -- Indexes for table `subdistrict`
@@ -283,7 +375,7 @@ ALTER TABLE `subdistrict`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `district`
@@ -304,10 +396,16 @@ ALTER TABLE `payment`
   MODIFY `PaymentID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `price`
+--
+ALTER TABLE `price`
+  MODIFY `priceId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `promotion`
 --
 ALTER TABLE `promotion`
-  MODIFY `PromotionID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `PromotionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `province`
@@ -319,7 +417,7 @@ ALTER TABLE `province`
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `ReservationID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ReservationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `role`
@@ -332,6 +430,12 @@ ALTER TABLE `role`
 --
 ALTER TABLE `store`
   MODIFY `StoreID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `storeprice`
+--
+ALTER TABLE `storeprice`
+  MODIFY `storepriceId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `subdistrict`
@@ -360,23 +464,28 @@ ALTER TABLE `employee`
 -- Constraints for table `payment`
 --
 ALTER TABLE `payment`
-  ADD CONSTRAINT `FK_Payment49` FOREIGN KEY (`PromotionID`) REFERENCES `promotion` (`PromotionID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_Payment51` FOREIGN KEY (`ReservationID`) REFERENCES `reservation` (`ReservationID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`customerId`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_Payment51` FOREIGN KEY (`ReservationID`) REFERENCES `reservation` (`ReservationID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reservation`
 --
 ALTER TABLE `reservation`
   ADD CONSTRAINT `FK_Reservation48` FOREIGN KEY (`PromotionID`) REFERENCES `promotion` (`PromotionID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_Reservation54` FOREIGN KEY (`StoreID`) REFERENCES `store` (`StoreID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerId`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerId`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`StorePriceID`) REFERENCES `storeprice` (`storepriceId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `store`
 --
 ALTER TABLE `store`
   ADD CONSTRAINT `FK_Store55` FOREIGN KEY (`subdistrictID`) REFERENCES `subdistrict` (`subdistrictID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `storeprice`
+--
+ALTER TABLE `storeprice`
+  ADD CONSTRAINT `storeprice_ibfk_1` FOREIGN KEY (`storeId`) REFERENCES `store` (`StoreID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `storeprice_ibfk_2` FOREIGN KEY (`priceId`) REFERENCES `price` (`priceId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `subdistrict`
